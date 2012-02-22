cf6208f5d85c76cf0579dd2ba235b180483d8157
Ryohei Fushimi <fushimi.ryohei@gmail.com>
2012-02-22 10:54:48 +0900

    bugfix about export of Box2dWeb.js

--- plugins/box2d.enchant.js
+++ plugins/box2d.enchant.js
@@ -33,27 +33,26 @@
  */
 
 
+if(!Box2D)throw new Error("box2d.enchant.js must be loaded after Box2dWeb.js");
+
+/* export */
+var b2Vec2 = Box2D.Common.Math.b2Vec2
+    , b2AABB = Box2D.Collision.b2AABB
+    , b2BodyDef = Box2D.Dynamics.b2BodyDef
+    , b2Body = Box2D.Dynamics.b2Body
+    , b2FixtureDef = Box2D.Dynamics.b2FixtureDef
+    , b2Fixture = Box2D.Dynamics.b2Fixture
+    , b2World = Box2D.Dynamics.b2World
+    , b2MassData = Box2D.Collision.Shapes.b2MassData
+    , b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
+    , b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
+    , b2DebugDraw = Box2D.Dynamics.b2DebugDraw
+    , b2MouseJointDef = Box2D.Dynamics.Joints.b2MouseJointDef;
+enchant.box2d = {};
+
 (function () {
-
-    enchant.box2d = {};
-
     var WORLD_SCALE = 32;
     var world;
-
-    var b2Vec2 = Box2D.Common.Math.b2Vec2
-        , b2AABB = Box2D.Collision.b2AABB
-        , b2BodyDef = Box2D.Dynamics.b2BodyDef
-        , b2Body = Box2D.Dynamics.b2Body
-        , b2FixtureDef = Box2D.Dynamics.b2FixtureDef
-        , b2Fixture = Box2D.Dynamics.b2Fixture
-        , b2World = Box2D.Dynamics.b2World
-        , b2MassData = Box2D.Collision.Shapes.b2MassData
-        , b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
-        , b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
-        , b2DebugDraw = Box2D.Dynamics.b2DebugDraw
-        , b2MouseJointDef = Box2D.Dynamics.Joints.b2MouseJointDef
-        ;
-
 
     /**
      * Spriteの種類（スタティック）
