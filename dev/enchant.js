2b75065e37d666c0e1802b3a7ce0d1805660a280
Ryohei Fushimi <fushimi.ryohe.i@gmail.com>
2012-06-28 18:46:21 +0900

    fix issue #67

--- dev/enchant.js
+++ dev/enchant.js
@@ -165,7 +165,7 @@ var RETINA_DISPLAY = (function() {
             viewport = document.createElement('meta');
             document.head.appendChild(viewport);
         }
-        viewport.setAttribute('content', 'width=640px');
+        viewport.setAttribute('content', 'width=640');
         return true;
     } else {
         return false;
