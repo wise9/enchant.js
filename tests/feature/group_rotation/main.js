a9858662134b6b8c0f380635f3502edfd400b60a
sidestepism <fushimi.ryohe.i@gmail.com>
2012-08-29 12:56:13 +0900

    add group_rotation test

--- tests/feature/group_rotation/main.js
+++ tests/feature/group_rotation/main.js
@@ -3,3 +3,30 @@
  */
 
 enchant();
+window.onload = function() {
+    var game = new Game(320, 320);
+    game.preload('chara1.png');
+    game.onload = function() {
+        var bear = new Sprite(32, 32);
+        bear.image = game.assets['chara1.png'];
+        bear.frame = 1;
+        bear.x = -32;
+
+        var white = new Sprite(32, 32);
+        white.image = game.assets['chara1.png'];
+        white.x = 32;
+        white.frame = 5;
+
+        var group = new enchant.Group();
+        group.addChild(bear);
+        group.addChild(white);
+
+        game.rootScene.addChild(group);
+        group.on('enterframe', function() {
+            this.rotation += 2;
+            this.x += 1;
+            this.y += 1;
+        });
+    };
+    game.start();
+};
