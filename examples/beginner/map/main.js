b95eec81a6238fb78b104c961fb969ed8398870d
sidestepism <fushimi.ryohe.i@gmail.com>
2012-09-07 15:34:14 +0900

    update map sample

--- examples/beginner/map/main.js
+++ examples/beginner/map/main.js
@@ -1,1 +1,15 @@
 enchant();
+
+window.onload = function() {
+    var game = new Game(320, 320);
+    game.fps = 20;
+    game.preload('chara1.png', 'map1.gif');
+    game.onload = function() {
+        var map = new Map(16, 16);
+        map.image = game.assets['map1.gif'];
+        map.loadData([[0,2,1,3,0]]);
+        game.rootScene.addChild(map);
+    };
+    game.start();
+};
+
