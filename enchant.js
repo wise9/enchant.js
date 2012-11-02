da84d3c2705b701e663136533e4013f7d2346b29
sidestepism <fushimi.ryohe.i@gmail.com>
2012-11-02 23:06:33 +0900

    throw error if superclass/definition is undefined
    Conflicts:
    
    	enchant.min.js

--- enchant.js
+++ enchant.js
@@ -219,6 +219,9 @@ enchant.Class = function(superclass, definition) {
  * @static
  */
 enchant.Class.create = function(superclass, definition) {
+    if (superclass == null){
+        throw new Error("superclass is undefined");
+    }
     if (arguments.length === 0) {
         return enchant.Class.create(Object, definition);
     } else if (arguments.length === 1 && typeof arguments[0] !== 'function') {
