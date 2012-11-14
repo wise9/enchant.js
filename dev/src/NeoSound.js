43f1d2e332df6f259a6a8f949df76916659f5828
Ryo Takahashi <at.rtsan@gmail.com>
2012-11-14 14:09:34 +0900

    update NeoSound

--- dev/src/NeoSound.js
+++ dev/src/NeoSound.js
@@ -10,31 +10,83 @@ if (webkitAudioContext) {
 
     enchant.NeoSound = enchant.Class.create(enchant.EventTarget, {
         initialize: function() {
+            var actx = enchant.NeoSound.audioContext;
             enchant.EventTarget.call(this);
-            this.src = null;
+            this.src = actx.createBufferSource();
             this.buffer = null;
-        },
-        load: function(src, callback) {
+            this._volume = 1;
+            this._currentTime = 0;
+            this._state = 0;
         },
         play: function() {
             var actx = enchant.NeoSound.audioContext;
-            this.src = actx.createBufferSource();
-            this.src.buffer = this.buffer;
-            this.src.noteOn(0);
-            this.src.connect(actx.destination);
+            if (this._state == 2) {
+                this.src.connect(actx.destination);
+            } else {
+                if (this._state == 1) {
+                    this.src.disconnect(actx.destination);
+                }
+                this.src = actx.createBufferSource();
+                this.src.buffer = this.buffer;
+                this.src.gain.value = this._volume;
+                this.src.connect(actx.destination);
+                this.src.noteOn(0);
+            }
+            this._state = 1;
         },
         pause: function() {
+            var actx = enchant.NeoSound.audioContext;
+            this.src.disconnect(actx.destination);
+            this._state = 2;
         },
         stop: function() {
+            this.src.noteOff(0);
+            this._state = 0;
         },
         clone: function() {
-            // iranaikamo
+            var sound = new enchant.NeoSound();
+            sound.buffer = this.buffer;
+            return sound;
+        },
+        dulation: {
+            get: function() {
+                if (this.buffer) {
+                    return this.buffer.dulation;
+                } else {
+                    return 0;
+                }
+            }
+        },
+        volume: {
+            get: function() {
+                return this._volume;
+            },
+            set: function(volume) {
+                volume = Math.max(0, Math.min(1, volume));
+                this._volume = volume;
+                if (this.src) {
+                    this.src.gain.value = volume;
+                }
+            }
+        },
+        currentTime: {
+            get: function() {
+                window.console.log('currentTime is not allowed');
+                return this._currentTime;
+            },
+            set: function(time) {
+                window.console.log('currentTime is not allowed');
+                this._currentTime = time;
+            }
         }
     });
+
     enchant.NeoSound.load = function(src, type, callback) {
         var actx = enchant.NeoSound.audioContext;
         var xhr = new XMLHttpRequest();
         var sound = new enchant.NeoSound();
+        var mimeType = 'audio/' + enchant.Game.findExt(src);
+        // TODO check Audio.canPlayType(mimeType)
         xhr.responseType = 'arraybuffer';
         xhr.open('GET', src, true);
         xhr.onload = function() {
@@ -45,8 +97,8 @@ if (webkitAudioContext) {
                     callback();
                 },
                 function(error) {
-                    console.log(error);
-                    callback();
+                    // TODO change to enchant Error
+                    window.console.log(error);
                 }
             );
         };
