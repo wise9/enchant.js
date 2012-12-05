981788746531b374ff69e812a4d337ebde3ab11e
chick307 <chick307@gmail.com>
2012-12-05 16:11:49 +0900

    Add a test case for the issue #95.

--- tests/qunit/enchant.js/entity.js
+++ tests/qunit/enchant.js/entity.js
@@ -117,3 +117,39 @@ test('Entity#intersect, #within between two entities in different Group', functi
     equal(a.intersect(b), true);
     equal(a.within(b), true);
 });
+
+/**
+ * @see https://github.com/wise9/enchant.js/issues/95
+ */
+test('Sprite should draw a image repetitively, ' +
+    'when the image is smaller than the sprite', function() {
+    var surface = new enchant.Surface(24, 24);
+    var context = surface.context;
+    // Draw checkboard pattern.
+    context.fillStyle = '#FFFFFF';
+    context.fillRect(0, 0, 24, 24);
+    context.fillStyle = '#000000';
+    context.fillRect(0, 0, 12, 12);
+    context.fillRect(12, 12, 12, 12);
+
+    var expected = new enchant.Surface(32, 32);
+    var ctx = expected.context;
+    ctx.fillStyle = '#FFFFFF';
+    ctx.fillRect(0, 0, 32, 32);
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(0, 0, 12, 12);
+    ctx.fillRect(12, 12, 12, 12);
+    ctx.fillRect(24, 0, 8, 12);
+    ctx.fillRect(0, 24, 12, 8);
+    ctx.fillRect(24, 24, 8, 8);
+    var expectedURL = expected._element.toDataURL();
+
+    var sprite = new enchant.Sprite(32, 32);
+    sprite.image = surface;
+
+    var actual = new enchant.Surface(32, 32);
+    sprite.cvsRender(actual.context);
+    var actualURL = actual._element.toDataURL();
+
+    equal(actualURL, expectedURL);
+});
