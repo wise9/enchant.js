7c89952914ee54a1f7277e9a8f67e81dfcd5e522
sidestepism <fushimi.ryohe.i@gmail.com>
2012-12-25 10:43:43 +0900

    fix #150
    Conflicts:
    
    	tests/qunit/enchant.js/label-textalign.js

---
+++ tests/qunit/enchant.js/label-textalign.js
@@ -0,0 +1,63 @@
+﻿var context, core, label;
+module('Label.textAlign', {
+    setup: function () {
+        enchant();
+        core = new Core(320, 320);
+        context = document.getElementsByTagName('canvas')[0].getContext('2d');
+
+        // using multibyte char in tests causes problem when running on phantomjs
+        label = new Label('*');
+        label.color = '#f00'
+        label.font = '10px monospace';
+
+        ok(!verifyTextAt('left'));
+        ok(!verifyTextAt('center'));
+        ok(!verifyTextAt('right'));
+    }
+});
+
+function verifyTextAt(textAlign) {
+    var x = 4, image;
+    if (textAlign === 'center') {
+        x = label.width / 2;
+    } else if (textAlign === 'right') {
+        x = label.width - 5;
+    }
+    image = context.getImageData(x,5,1,1).data;
+    return image[0] === 255;
+}
+
+test('textAlign supports "left"', function (){
+    label.textAlign = 'left';
+    core.rootScene.addChild(label);
+
+    ok( verifyTextAt('left'));
+    ok(!verifyTextAt('center'));
+    ok(!verifyTextAt('right'));
+});
+
+test('textAlign supports "center"', function (){
+    label.textAlign = 'center';
+    core.rootScene.addChild(label);
+
+    ok(!verifyTextAt('left'));
+    ok( verifyTextAt('center'));
+    ok(!verifyTextAt('right'));
+});
+
+test('textAlign supports "right"', function (){
+    label.textAlign = 'right';
+    core.rootScene.addChild(label);
+
+    ok(!verifyTextAt('left'));
+    ok(!verifyTextAt('center'));
+    ok( verifyTextAt('right'));
+});
+
+test('default textAlign is "left"', function (){
+    core.rootScene.addChild(label);
+
+    ok( verifyTextAt('left'));
+    ok(!verifyTextAt('center'));
+    ok(!verifyTextAt('right'));
+});
