b9420eb4ffb5c8ddb917c83b91226309000522d7
sidestepism <fushimi.ryohe.i@gmail.com>
2013-01-07 17:42:28 +0900

    test commit for qunit issue (#154)

--- tests/qunit/avoidTimeout.js
+++
@@ -1,8 +0,0 @@
-var times = 0;
-var __id = setInterval(function() {
-    console.log(''); // dummy for grunt qunit
-    times++;
-    if (times > 30) {
-        clearInterval(__id);
-    }
-}, 1000);
