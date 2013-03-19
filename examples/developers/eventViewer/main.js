c30a1a0071480b2f13d698e35314f1c8f0899b1a
sidestepism <fushimi.ryohe.i@gmail.com>
2013-03-19 22:35:32 +0900

    add eventViewer sample

---
+++ examples/developers/eventViewer/main.js
@@ -0,0 +1,37 @@
+enchant();
+
+window.onload = function(){
+    var game = new Game(320, 320);
+    game.onload = function(){
+        var status = new Label("");
+        status.add = function(str){
+            this.text = str + "<br />" + this.text;
+        }
+        
+        var round = function(num){
+            return Math.round(num * 1e3)/1e3;
+        }
+        
+        game.rootScene.on('touchstart', function(evt){
+            status.add('touchstart (' + round(evt.x) + ', ' + round(evt.y) + ')');
+        });
+        game.rootScene.on('touchmove', function(evt){
+            status.add('touchmove (' + round(evt.x) + ', ' + round(evt.y) + ')');
+        });
+        game.rootScene.on('touchstart', function(evt){
+            status.add('touchend (' + round(evt.x) + ', ' + round(evt.y) + ')');
+        });
+        
+        ['up', 'down', 'right', 'left'].forEach(function (direction){
+            var d = direction;
+            game.rootScene.on(direction + 'buttondown', function(){
+                status.add(d + 'buttondown');
+            })
+            game.rootScene.on(direction + 'buttonup', function(){
+                status.add(d + 'buttonup');
+            })
+        });
+        game.rootScene.addChild(status);
+    };
+    game.start();
+}
