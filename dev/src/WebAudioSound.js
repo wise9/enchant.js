3a71070a9e5c6c5f30e1393345122bd71a099f05
kumabook <kumabook@live.jp>
2013-11-10 23:38:56 +0900

    Fix deprecated web-audio api: GainNode.gain.value instead of AudioBufferSourceNode.gain. AudioBufferSourceNode.start() instead of AudioBufferSourceNode.noteGrainOn(). AudioBufferSourceNode.stop() instead of AudioBufferSourceNode.noteOff().

--- dev/src/WebAudioSound.js
+++ dev/src/WebAudioSound.js
@@ -55,10 +55,13 @@ enchant.WebAudioSound = enchant.Class.create(enchant.EventTarget, {
         var offset = this._currentTime;
         var actx = this.context;
         this.src = actx.createBufferSource();
+        this._gain = actx.createGain();
         this.src.buffer = this.buffer;
-        this.src.gain.value = this._volume;
-        this.src.connect(this.connectTarget);
-        this.src.noteGrainOn(0, offset, this.buffer.duration - offset - 1.192e-7);
+        this._gain.gain.value = this._volume;
+
+        this.src.connect(this._gain);
+        this._gain.connect(this.connectTarget);
+        this.src.start(0, offset, this.buffer.duration - offset - 1.192e-7);
         this._startTime = actx.currentTime - this._currentTime;
         this._state = 1;
     },
@@ -78,7 +81,7 @@ enchant.WebAudioSound = enchant.Class.create(enchant.EventTarget, {
         if (currentTime === this.duration) {
             return;
         }
-        this.src.noteOff(0);
+        this.src.stop(0);
         this._currentTime = currentTime;
         this._state = 2;
     },
@@ -94,7 +97,7 @@ enchant.WebAudioSound = enchant.Class.create(enchant.EventTarget, {
      [/lang]
      */
     stop: function() {
-        this.src.noteOff(0);
+        this.src.stop(0);
         this._state = 0;
     },
     /**
@@ -157,7 +160,7 @@ enchant.WebAudioSound = enchant.Class.create(enchant.EventTarget, {
             volume = Math.max(0, Math.min(1, volume));
             this._volume = volume;
             if (this.src) {
-                this.src.gain.value = volume;
+		this._gain.gain.value = volume;
             }
         }
     },
