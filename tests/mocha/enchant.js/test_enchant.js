e9a5aef78586b5738fcfeb0f77bcf59f4eb58f64
Yusuke HIDESHIMA <yusuke.hideshima@uei.co.jp>
2014-01-30 17:50:20 +0900

    add Class#create creates indivisual class test

---
+++ tests/mocha/enchant.js/test_enchant.js
@@ -0,0 +1,9 @@
+describe("enchant.js", function(){
+    afterEach(function(){
+    });
+
+    describe("export", function(){
+        it("should export globally", function(){
+        });
+    });
+});
